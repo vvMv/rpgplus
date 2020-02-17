13687877db5cd8d1d6392557096530d60a324b12
vvMv <38408970+vvMv@users.noreply.github.com>
2020-02-17 15:30:47 +0000

    Reintroduced commented SplitShot code for testing

--- src/main/java/com/vmv/rpgplus/skill/archery/SplitShot.java
+++ src/main/java/com/vmv/rpgplus/skill/archery/SplitShot.java
@@ -1,8 +1,15 @@
 package com.vmv.rpgplus.skill.archery;
 
+import com.vmv.rpgplus.player.RPGPlayerManager;
 import com.vmv.rpgplus.skill.Ability;
 import com.vmv.rpgplus.skill.SkillType;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
 
 public class SplitShot extends Ability implements Listener {
 
@@ -15,28 +22,28 @@ public class SplitShot extends Ability implements Listener {
         this.cooldown = 0;
     }
 
-//    @EventHandler
-//    public void onBowShoot(EntityShootBowEvent e) {
-//
-//        if (!checkReady(e.getEntity())) return;
-//
-//        double playerLevel = RPGPlayerManager.getInstance().getPlayer((Player) e.getEntity()).getLevel(SkillType.ARCHERY);
-//        int amount = (int) (playerLevel / extra) + 1;
-//        int angle = getAbilityConfigSection().getInt("angle");
-//        int separation = angle / amount;
-//
-//        Location loc = e.getEntity().getLocation();
-//        for (int i = 1; i < amount; i++) {
-//            loc.setYaw((e.getEntity().getLocation().getYaw() -(angle/2)) + (i * separation));
-//            Arrow a = e.getEntity().launchProjectile(Arrow.class);
-//            a.setVelocity(loc.getDirection());
-//            a.setShooter(e.getEntity());
-//            a.setPickupStatus(Arrow.PickupStatus.CREATIVE_ONLY);
-//            a.setCustomName("splitshot_arrow");
-//            a.setBounce(false);
-//            a.setCritical(true);
-//            a.getWorld().playSound(a.getLocation(), Sound.ENTITY_ARROW_SHOOT, 1.5F, 1.0F * 0.15F);
-//        }
-//        e.setCancelled(true);
-//    }
+    @EventHandler
+    public void onBowShoot(EntityShootBowEvent e) {
+
+        if (!checkReady(e.getEntity())) return;
+
+        double playerLevel = RPGPlayerManager.getInstance().getPlayer((Player) e.getEntity()).getLevel(SkillType.ARCHERY);
+        int amount = (int) (playerLevel / extra) + 1;
+        int angle = getAbilityConfigSection().getInt("angle");
+        int separation = angle / amount;
+
+        Location loc = e.getEntity().getLocation();
+        for (int i = 1; i < amount; i++) {
+            loc.setYaw((e.getEntity().getLocation().getYaw() -(angle/2)) + (i * separation));
+            Arrow a = e.getEntity().launchProjectile(Arrow.class);
+            a.setVelocity(loc.getDirection());
+            a.setShooter(e.getEntity());
+            a.setPickupStatus(Arrow.PickupStatus.CREATIVE_ONLY);
+            a.setCustomName("splitshot_arrow");
+            a.setBounce(false);
+            a.setCritical(true);
+            a.getWorld().playSound(a.getLocation(), Sound.ENTITY_ARROW_SHOOT, 1.5F, 1.0F * 0.15F);
+        }
+        e.setCancelled(true);
+    }
 }
