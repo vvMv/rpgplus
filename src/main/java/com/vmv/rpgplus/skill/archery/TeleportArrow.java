67aaffd415a3d3bf58ed7b5c0a519b3c1d6ffb3a
Ogbli <oliver.overton123@gmail.com>
2020-02-18 23:28:00 +0000

    Updated a time before the teleport arrow becomes invalid. This is to avoid it going into unloaded chunks for later use.

--- src/main/java/com/vmv/rpgplus/skill/archery/TeleportArrow.java
+++ src/main/java/com/vmv/rpgplus/skill/archery/TeleportArrow.java
@@ -3,6 +3,7 @@ package com.vmv.rpgplus.skill.archery;
 import com.vmv.rpgplus.skill.Ability;
 import com.vmv.rpgplus.skill.SkillType;
 import org.bukkit.Location;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 
 public class TeleportArrow extends Ability implements Listener {
 
+    private static final int TELEPORT_INVALID_TIME = 200;
     private ArrayList<Projectile> arrows = new ArrayList<Projectile>();
 
     public TeleportArrow(String name, SkillType st) {
@@ -34,12 +36,13 @@ public class TeleportArrow extends Ability implements Listener {
     @EventHandler
     public void onArrowLand(ProjectileHitEvent e) {
         if (arrows.contains(e.getEntity())) {
+            arrows.remove(e.getEntity());
+            if (e.getEntity().getTicksLived() > TELEPORT_INVALID_TIME) return;
             Player p = (Player) e.getEntity().getShooter();
             Location loc = e.getEntity().getLocation();
             p.teleport(new Location(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ(), p.getLocation().getYaw(), p.getLocation().getPitch()));
             p.damage(getAbilityConfigSection().getInt("selfdamage"));
             e.getEntity().setGlowing(false);
-            arrows.remove(e.getEntity());
         }
     }
 }
