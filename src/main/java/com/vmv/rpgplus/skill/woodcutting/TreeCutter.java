bbcfb23375d25c7dcf150a22a9764315911673f1
vvMv <38408970+vvMv@users.noreply.github.com>
2020-03-23 16:29:48 +0000

    Added a maximum size for treefeller ability configuration "maxsize:"

--- src/main/java/com/vmv/rpgplus/skill/woodcutting/TreeCutter.java
+++ src/main/java/com/vmv/rpgplus/skill/woodcutting/TreeCutter.java
@@ -1,19 +1,18 @@
 package com.vmv.rpgplus.skill.woodcutting;
 
-import com.vmv.core.information.InformationHandler;
-import com.vmv.core.information.InformationType;
 import com.vmv.rpgplus.main.RPGPlus;
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitScheduler;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class TreeCutter extends BukkitRunnable {
 
@@ -23,18 +22,20 @@ public class TreeCutter extends BukkitRunnable {
     private List<String> comparisonBlockArrayLeaves = new ArrayList<>();
     private List<Block> blocks = new ArrayList<>();
     private List<Block> leaves = new ArrayList<>();
+    private int maxsize;
     private double delay;
 
-    public TreeCutter(Player player, Block startBlock, double delay) {
+    public TreeCutter(Player player, Block startBlock, double delay, int maxsize) {
         this.delay = delay;
         this.player = player;
         this.startBlock = startBlock;
+        this.maxsize = maxsize;
     }
 
     @Override
     public void run() {
         blocks.add(startBlock);
-        runBlockLoop(startBlock, startBlock.getX(), startBlock.getZ());
+        runBlockLoop(startBlock, startBlock.getX(), startBlock.getZ(), 0);
 
         int c = 0;
         for (Block b : blocks) {
@@ -55,13 +56,14 @@ public class TreeCutter extends BukkitRunnable {
         this.cancel();
     }
 
-    public void runBlockLoop(Block b1, final int x1, final int z1) {
+    public void runBlockLoop(Block b1, final int x1, final int z1, int size) {
         for (int x = -2; x <= 2; x++) {
             for (int y = -2; y <= 2; y++) {
                 for (int z = -2; z <= 1; z++) {
                     if (x == 0 && y == 0 && z == 0)
                         continue;
 
+                    if (size >= maxsize) break;
                     Block b2 = b1.getRelative(x, y, z);
                     String s = b2.getX() + ":" + b2.getY() + ":" + b2.getZ();
 
@@ -76,7 +78,7 @@ public class TreeCutter extends BukkitRunnable {
                     if (!comparisonBlockArray.contains(s)) {
                         comparisonBlockArray.add(s);
                         blocks.add(b2);
-                        this.runBlockLoop(b2, x1, z1);
+                        this.runBlockLoop(b2, x1, z1, size++);
                     }
                 }
             }
--- src/main/java/com/vmv/rpgplus/skill/woodcutting/TreeFeller.java
+++ src/main/java/com/vmv/rpgplus/skill/woodcutting/TreeFeller.java
@@ -11,18 +11,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TreeFeller extends Ability implements Listener {
 
     public static List<Material> leaves = new ArrayList<>();
     public static List<Material> logs = new ArrayList<>();
 
+    private int maxsize;
     private double delay;
 
     public TreeFeller(String name, SkillType st, AbilityAttribute... attributes) {
         super(name, st, attributes);
         delay = getAbilityConfigSection().getDouble("delay");
+        maxsize = getAbilityConfigSection().getInt("maxsize");
         try { getAbilityConfigSection().getStringList("logs").forEach(b -> logs.add(Material.valueOf(b))); } catch (IllegalArgumentException e) { InformationHandler.printMessage(InformationType.ERROR, "Invalid value at ability.tree_feller.logs", e.getMessage(), "This error is coming from woodcutting.yml" ); }
         try { getAbilityConfigSection().getStringList("leaves").forEach(b -> leaves.add(Material.valueOf(b))); } catch (IllegalArgumentException e) { InformationHandler.printMessage(InformationType.ERROR, "Invalid value at ability.tree_feller.leaves", e.getMessage(), "This error is coming from woodcutting.yml" ); }
     }
@@ -39,7 +42,7 @@ public class TreeFeller extends Ability implements Listener {
         }
 
         if (isActive(e.getPlayer())) {
-            new TreeCutter(e.getPlayer(), e.getBlock(), delay).runTaskAsynchronously(RPGPlus.getInstance());
+            new TreeCutter(e.getPlayer(), e.getBlock(), delay, maxsize).runTaskAsynchronously(RPGPlus.getInstance());
             e.setCancelled(true);
         }
 
