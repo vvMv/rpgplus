7860f64eb482a2115aa85a684caae39e7796866c
vvMv <38408970+vvMv@users.noreply.github.com>
2020-03-25 13:40:59 +0000

    Fixed ability "enabled" in skill configurations

--- src/main/java/com/vmv/rpgplus/skill/AbilityManager.java
+++ src/main/java/com/vmv/rpgplus/skill/AbilityManager.java
@@ -1,25 +1,11 @@
 package com.vmv.rpgplus.skill;
 
-import com.vmv.core.information.InformationHandler;
-import com.vmv.core.information.InformationType;
 import com.vmv.rpgplus.main.RPGPlus;
-import com.vmv.rpgplus.skill.archery.ExplosiveArrow;
-import com.vmv.rpgplus.skill.archery.MultiArrow;
-import com.vmv.rpgplus.skill.archery.SplitShot;
-import com.vmv.rpgplus.skill.archery.TeleportArrow;
-import com.vmv.rpgplus.skill.attack.Track;
-import com.vmv.rpgplus.skill.mining.OreLocator;
-import com.vmv.rpgplus.skill.mining.VeinMiner;
-import com.vmv.rpgplus.skill.stamina.Dash;
-import com.vmv.rpgplus.skill.woodcutting.TreeFeller;
-import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static com.vmv.rpgplus.skill.SkillType.*;
 
 public class AbilityManager implements Listener {
 
@@ -38,7 +24,7 @@ public class AbilityManager implements Listener {
 
     public static void registerAbility(Listener... ab) {
         for (Listener a : ab) {
-            if (((Ability) a).getSkillConfig().getBoolean("enabled")) {
+            if (((Ability) a).isEnabled()) {
                 RPGPlus.getInstance().registerEvents(a);
                 abilities.add((Ability) a);
             }
