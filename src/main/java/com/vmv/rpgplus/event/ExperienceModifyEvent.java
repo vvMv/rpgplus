987ba32105f1d7147684f67efbf254e6d2981fb7
vvMv <38408970+vvMv@users.noreply.github.com>
2020-03-25 18:57:36 +0000

    Small format changes

--- src/main/java/com/vmv/rpgplus/event/ExperienceModifyEvent.java
+++ src/main/java/com/vmv/rpgplus/event/ExperienceModifyEvent.java
@@ -1,24 +1,15 @@
 package com.vmv.rpgplus.event;
 
 import com.vmv.core.math.MathUtils;
-import com.vmv.rpgplus.database.PlayerSetting;
-import com.vmv.rpgplus.main.RPGPlus;
 import com.vmv.rpgplus.player.RPGPlayer;
-import com.vmv.rpgplus.player.RPGPlayerManager;
-import com.vmv.rpgplus.skill.SkillManager;
 import com.vmv.rpgplus.skill.SkillType;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class ExperienceModifyEvent extends PlayerEvent implements Cancellable {
 
@@ -35,7 +26,6 @@ public class ExperienceModifyEvent extends PlayerEvent implements Cancellable {
         this.rp = rp;
         this.skill = skill;
         this.exp = MathUtils.round(exp, 2);
-        spawnExperienceAnimation(1);
     }
 
     public double getExp() {
@@ -71,35 +61,6 @@ public class ExperienceModifyEvent extends PlayerEvent implements Cancellable {
 
     public static HandlerList getHandlerList() {
         return handlers;
-    }
-
-    private void spawnExperienceAnimation(double duration) {
-        if (!RPGPlus.getInstance().getConfig().getBoolean("general.experience_animation")) return;
-        if (player == null || player.getGameMode() == GameMode.SPECTATOR || player.hasPotionEffect(PotionEffectType.INVISIBILITY)) return;
-        if (!RPGPlayerManager.getInstance().getPlayer(player).getSettingBoolean(PlayerSetting.EXPERIENCE_POPUPS)) return;
-        ArmorStand as = player.getWorld().spawn(getVariateLocation(player.getLocation()), ArmorStand.class);
-        as.setVisible(false);
-        as.setSmall(true);
-        as.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, (int) (duration*20), 1));
-        as.setCustomNameVisible(true);
-        as.setCustomName(SkillManager.getInstance().getSkill(getSkill()).getSkillColor() + "+" + getExp() + "xp");
-
-        animationStands.add(as);
-
-        Bukkit.getScheduler().runTaskLater(RPGPlus.getInstance(), new Runnable() {
-            public void run() {
-                animationStands.remove(as);
-                as.remove();
-            }
-        }, (long) (duration * 20));
-    }
-
-    private Location getVariateLocation(Location l) {
-        Random r = new Random();
-        l.setYaw(r.nextInt() > 50 ? l.getYaw() - MathUtils.getRandom(25, 15) : l.getYaw() - -MathUtils.getRandom(25, 15));
-        l.setPitch(l.getPitch() + MathUtils.getRandom(-6, -12));
-        l.add(l.getDirection().multiply(5));
-        return l;
     }
 
     public static ArrayList<ArmorStand> getAnimationStands() {
