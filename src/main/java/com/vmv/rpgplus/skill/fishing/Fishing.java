5261419dd0f0ae6b43416eb3eed7bb90a4fa1266
vvMv <38408970+vvMv@users.noreply.github.com>
2020-03-26 15:26:52 +0000

    Removed code that shouldn't have been there

--- src/main/java/com/vmv/rpgplus/skill/fishing/Fishing.java
+++ src/main/java/com/vmv/rpgplus/skill/fishing/Fishing.java
@@ -4,13 +4,9 @@ import com.vmv.core.math.MathUtils;
 import com.vmv.rpgplus.player.RPGPlayerManager;
 import com.vmv.rpgplus.skill.Skill;
 import com.vmv.rpgplus.skill.SkillType;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
-
-import java.util.HashMap;
 
 public class Fishing extends Skill implements Listener {
 
@@ -45,17 +41,6 @@ public class Fishing extends Skill implements Listener {
                     break;
                 }
             }
-
-
-            HashMap<Player, Integer> allCanes = new HashMap<>();
-
-            for (String configPlayer : getConfig().getConfigurationSection("Canes").getKeys(false)) {
-                Player p = Bukkit.getPlayer(configPlayer);
-                int amount = getConfig().getInt("Canes." + configPlayer);
-                allCanes.put(p, amount);
-            }
-
-
 
             RPGPlayerManager.getInstance().getPlayer(e.getPlayer()).addXP(SkillType.FISHING, xp);
         }
