b9a6f9201eb2d2dc8465a2394e14ae4873ddfb07
vvMv <38408970+vvMv@users.noreply.github.com>
2020-04-16 12:07:19 +0100

    Fix ArrayIndexOutOfBounds exception being thrown when StringBuilder is empty because player has no points to spend and reminder message is empty

--- src/main/java/com/vmv/rpgplus/player/RPGPlayer.java
+++ src/main/java/com/vmv/rpgplus/player/RPGPlayer.java
@@ -254,7 +254,7 @@ public class RPGPlayer {
                 sb.append(WordUtils.capitalizeFully(skill.getFormattedName()) + " " + (int) getAbilityPoints(skill) + ", ");
             }
 
-            sb.deleteCharAt(sb.length() - 2);
+            if (sb.length() >= 2) sb.deleteCharAt(sb.length() - 2);
 
             for (String s : reminder) {
                 ChatUtil.sendCenteredChatMessage(Bukkit.getPlayer(getUuid()), s.replaceAll("%a", sb.toString()));
