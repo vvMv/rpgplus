4a19333bc6223da3318d803cbc9a96298ad6e85d
vvMv <38408970+vvMv@users.noreply.github.com>
2020-08-24 12:00:12 +0100

    Centered menu GUI

--- src/main/java/com/vmv/rpgplus/inventory/RPGMenu.java
+++ src/main/java/com/vmv/rpgplus/inventory/RPGMenu.java
@@ -15,15 +15,15 @@ public class RPGMenu implements InventoryProvider {
     public void init(Player player, InventoryContents inventoryContents) {
         RPGPlayer rp = RPGPlayerManager.getInstance().getPlayer(player);
         inventoryContents.fill(ClickableItem.empty(InventoryUtils.getItem(XMaterial.GRAY_STAINED_GLASS_PANE)));
-        inventoryContents.set(1, 1, ClickableItem.of(InventoryUtils.getItem(XMaterial.BLAZE_POWDER, FileManager.getLang().getString("inventory.item_abilities"), 1, FileManager.getLang().getString("inventory.item_abilities_lore")), e -> {
+        inventoryContents.set(1, 3, ClickableItem.of(InventoryUtils.getItem(XMaterial.BLAZE_POWDER, FileManager.getLang().getString("inventory.item_abilities"), 1, FileManager.getLang().getString("inventory.item_abilities_lore")), e -> {
             InventoryUtils.sendClickSound(player);
             InventoryUtils.getInventory(InventoryUtils.getTitle("inventory.title_abilities", player), 6, new RPGAbilitiesMenu(), "").open(player);
         }));
-        inventoryContents.set(1, 3, ClickableItem.of(InventoryUtils.getItem(XMaterial.ENDER_EYE, FileManager.getLang().getString("inventory.item_ability_points"), 1, FileManager.getLang().getString("inventory.item_ability_points_lore").replace("%n", String.valueOf((int)rp.getAbilityPoints()))), e -> {
+        inventoryContents.set(1, 5, ClickableItem.of(InventoryUtils.getItem(XMaterial.ENDER_EYE, FileManager.getLang().getString("inventory.item_ability_points"), 1, FileManager.getLang().getString("inventory.item_ability_points_lore").replace("%n", String.valueOf((int)rp.getAbilityPoints()))), e -> {
             InventoryUtils.sendClickSound(player);
             InventoryUtils.getInventory(InventoryUtils.getTitle("inventory.title_select_skill", player), 1, new RPGSelectPointsMenu(), "").open(player);
         }));
-        inventoryContents.set(1, 5, ClickableItem.of(InventoryUtils.getItem(XMaterial.PAPER, FileManager.getLang().getString("inventory.item_notifications"), 1, FileManager.getLang().getString("inventory.item_notifications_lore")), e -> {
+        inventoryContents.set(1, 8, ClickableItem.of(InventoryUtils.getItem(XMaterial.PAPER, FileManager.getLang().getString("inventory.item_notifications"), 1, FileManager.getLang().getString("inventory.item_notifications_lore")), e -> {
             InventoryUtils.sendClickSound(player);
             InventoryUtils.getInventory(InventoryUtils.getTitle("inventory.title_notifications", player), 3, new RPGNotificationsMenu(), "").open(player);
         }));
